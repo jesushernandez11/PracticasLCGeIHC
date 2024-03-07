e66d1f393480e4fe3f99f5f43e4db8461b99c28b
Jesus Hernandez <jesus_hernandez11@comunidad.unam.mx>
2024-03-06 18:55:57 -0600

    feat: Adding the return metods for articulacion7 and articulacion8

--- Window.h
+++ Window.h
@@ -26,12 +26,14 @@ public:
 	GLfloat getarticulacion4() { return articulacion4; }
 	GLfloat getarticulacion5() { return articulacion5; }
 	GLfloat getarticulacion6() { return articulacion6; }
+	GLfloat getArticulacion7() { return articulacion7; }
+	GLfloat getArticulacion8() { return articulacion8; }
 
 	~Window();
 private: 
 	GLFWwindow *mainWindow;
 	GLint width, height;
-	GLfloat rotax,rotay,rotaz, articulacion1, articulacion2, articulacion3, articulacion4, articulacion5, articulacion6;
+	GLfloat rotax,rotay,rotaz, articulacion1, articulacion2, articulacion3, articulacion4, articulacion5, articulacion6, articulacion7, articulacion8;
 	bool keys[1024];
 	GLint bufferWidth, bufferHeight;
 	GLfloat lastX;
